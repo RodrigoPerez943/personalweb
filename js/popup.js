41c961ab31b41778a60e2ce01778eea7be62e41e
RodrigoPerez943 <rodrigo.palmodovar@alumnos.upm.es>
2024-07-15 01:31:46 +0200

    Pop Up Mantenimiento

---
+++ js/popup.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    document.getElementById('popup').style.display = 'flex';
+};
+
+// Función para cerrar el popup
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
